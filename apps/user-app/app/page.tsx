7ba7ac2aed78c92955441952966794b0085824e6
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2024-03-28 14:31:21 +0530

    project init

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Code } from "@repo/ui/code";
+import styles from "./page.module.css";
+import { Button } from "@repo/ui/button";
+
+
+export default function Page(): JSX.Element {
+  return (
+    <h1 className="font-bold text-2xl">hiii</h1>
+  );
+}
