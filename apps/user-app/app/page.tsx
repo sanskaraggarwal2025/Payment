9d25b2d2d6164009c77e0394e1e714264c7d0f14
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2024-03-31 17:30:24 +0530

    recoil issue done

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,10 +3,12 @@ import { Card } from "@repo/ui/card";
 import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
-
-
+import Test from "./test";
 export default function Page(): JSX.Element {
   return (
-    <h1 className="font-bold text-2xl">hiii</h1>
+    <>
+      <h1 className="font-bold text-2xl">hiii</h1>
+      <Test />
+    </>
   );
 }
