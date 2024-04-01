5e747c1d92fb5b6aa352f4f21bcddde252bdca75
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2024-04-01 18:07:20 +0530

    authentication doen

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
-import Test from "./test";
-export default function Page(): JSX.Element {
+"use client"
+
+import {signIn, signOut, useSession} from "next-auth/react";
+import {Appbar} from "@repo/ui/appbar";
+
+export default function Page():JSX.Element {
+  const session = useSession();
   return (
-    <>
-      <h1 className="font-bold text-2xl">hiii</h1>
-      <Test />
-    </>
-  );
+    <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user = {session.data?.user} />
+    </div>
+  )
 }
