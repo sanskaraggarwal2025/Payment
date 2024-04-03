4a874267791a18464b7dd3fefcf4f073807ade02
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2024-04-03 19:52:44 +0530

    p2p transaction

---
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -0,0 +1,54 @@
+"use server"
+
+import { getServerSession } from "next-auth"
+import { authOptions } from "../auth"
+import prisma from "@repo/db/client"
+
+export async function p2pTransfer(to: string, amount: number) {
+
+    const session = await getServerSession(authOptions);
+    const from = session?.user?.id;
+    if (!from) {
+        return {
+            msg: "error while sending"
+        }
+    }
+
+    const toUser = await prisma.user.findFirst({
+        where: {
+            number: to
+        }
+    });
+
+    if (!toUser) {
+        return {
+            msg: "error while sending"
+        }
+    }
+
+    await prisma.$transaction(async (tx) => {
+        const fromBalance = await tx.balance.findUnique({
+            where: { userId: Number(from) }
+        })
+
+        if (!fromBalance || fromBalance.amount < amount) {
+            throw new Error('Insufficient fund');
+        }
+
+        await tx.balance.update({
+            where: { userId: Number(from) },
+            data: { amount: { decrement: amount } }
+        })
+
+        await tx.balance.update({
+            where: {
+                userId: toUser.id
+            },
+            data: {
+                amount: {
+                    increment: amount
+                }
+            }
+        })
+    })
+}
