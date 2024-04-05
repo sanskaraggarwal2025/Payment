105832263756afb0939a8c248e78e2a14f72077f
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2024-04-05 10:44:49 +0530

    transaction page added

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -1,56 +1,9 @@
 import { SendCard } from "../../../components/SendCard";
 
-import prisma from "@repo/db/client";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../lib/auth";
-
-async function getP2pTransactions() {
-  const session = await getServerSession(authOptions);
-  const currentUserId = Number(session?.user?.id);
-  const txns = await prisma.p2pTransfer.findMany({
-    where: {
-      OR: [
-        { fromUserId: Number(session?.user?.id) },
-        { toUserId: Number(session?.user?.id) },
-      ],
-    },
-    include: {
-      fromUser: { select: { number: true } },
-      toUser: { select: { number: true } },
-    },
-  });
-  console.log("line 17", txns);
-
-  return txns.map((t) => {
-    let sign = "";
-    if (t.fromUserId === currentUserId) {
-      sign = "-";
-    } else if (t.toUserId === currentUserId) {
-      sign = "+";
-    }
-    return {
-      amount: t.amount,
-      fromUserNumber: t.fromUser.number,
-      toUserNumber: t.toUser.number,
-      sign: sign,
-    };
-  });
-}
 export default async function () {
-  const history = await getP2pTransactions();
-
   return (
-    <div className="w-full">
+    <div className="w-full ">
       <SendCard />
-      {history.map((t) => (
-        <>
-          <h1>
-            {t.sign}
-            {t.amount / 100}
-          </h1>
-          <h1>{t.toUserNumber}</h1>
-        </>
-      ))}
     </div>
   );
 }
--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,64 @@
+import { Card } from "@repo/ui/card";
+import prisma from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { Center } from "@repo/ui/center";
+async function getP2pTransactions() {
+  const session = await getServerSession(authOptions);
+  const currentUserId = Number(session?.user?.id);
+  const txns = await prisma.p2pTransfer.findMany({
+    where: {
+      OR: [
+        { fromUserId: Number(session?.user?.id) },
+        { toUserId: Number(session?.user?.id) },
+      ],
+    },
+    include: {
+      fromUser: { select: { name: true } },
+      toUser: { select: { name: true } },
+    },
+  });
+  console.log("line 17", txns);
 
-export default function() {
-    return <div>
-        Transaction Page 
+  return txns.map((t) => {
+    let sign = "";
+    if (t.fromUserId === currentUserId) {
+      sign = "-";
+    } else if (t.toUserId === currentUserId) {
+      sign = "+";
+    }
+    return {
+      amount: t.amount,
+      fromUserName: t.fromUser.name,
+      toUserName: t.toUser.name,
+      sign: sign,
+    };
+  });
+}
+export default async function () {
+  const history = await getP2pTransactions();
+
+  return (
+    <div className="w-full">
+    <div className="h-[90vh]">
+      <Center>
+        <Card title="Recent Transactions">
+          <div className="min-w-72 pt-2">
+            {history.map((t) => (
+              <>
+                <div>
+                  <div className="text-sm">Received INR</div>
+                  <div className="text-slate-600 text-xs">{t.toUserName}</div>
+                </div>
+                <div className="flex flex-col justify-center">
+                  {t.sign} Rs {t.amount / 100}
+                </div>
+              </>
+            ))}
+          </div>
+        </Card>
+      </Center>
     </div>
+    </div>
+  );
 }
